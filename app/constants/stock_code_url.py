27703468821e8d921cbef9a17e03cdaf4f6b2bca
TIANXIANGYANGYYDS <1265918306@qq.com>
2026-04-19 04:17:35 +0800

    add stock code url config

---
+++ app/constants/stock_code_url.py
@@ -0,0 +1,452 @@
+skock_code_urls_map = [
+  {
+    "sector_name": "种植业与林业",
+    "sector_code": "881101",
+    "detail_url": "http://q.10jqka.com.cn/thshy/detail/code/881101/"
+  },
+  {
+    "sector_name": "养殖业",
+    "sector_code": "881102",
+    "detail_url": "http://q.10jqka.com.cn/thshy/detail/code/881102/"
+  },
+  {
+    "sector_name": "农产品加工",
+    "sector_code": "881103",
+    "detail_url": "http://q.10jqka.com.cn/thshy/detail/code/881103/"
+  },
+  {
+    "sector_name": "煤炭开采加工",
+    "sector_code": "881105",
+    "detail_url": "http://q.10jqka.com.cn/thshy/detail/code/881105/"
+  },
+  {
+    "sector_name": "油气开采及服务",
+    "sector_code": "881107",
+    "detail_url": "http://q.10jqka.com.cn/thshy/detail/code/881107/"
+  },
+  {
+    "sector_name": "化学原料",
+    "sector_code": "881108",
+    "detail_url": "http://q.10jqka.com.cn/thshy/detail/code/881108/"
+  },
+  {
+    "sector_name": "化学制品",
+    "sector_code": "881109",
+    "detail_url": "http://q.10jqka.com.cn/thshy/detail/code/881109/"
+  },
+  {
+    "sector_name": "钢铁",
+    "sector_code": "881112",
+    "detail_url": "http://q.10jqka.com.cn/thshy/detail/code/881112/"
+  },
+  {
+    "sector_name": "金属新材料",
+    "sector_code": "881114",
+    "detail_url": "http://q.10jqka.com.cn/thshy/detail/code/881114/"
+  },
+  {
+    "sector_name": "建筑材料",
+    "sector_code": "881115",
+    "detail_url": "http://q.10jqka.com.cn/thshy/detail/code/881115/"
+  },
+  {
+    "sector_name": "建筑装饰",
+    "sector_code": "881116",
+    "detail_url": "http://q.10jqka.com.cn/thshy/detail/code/881116/"
+  },
+  {
+    "sector_name": "通用设备",
+    "sector_code": "881117",
+    "detail_url": "http://q.10jqka.com.cn/thshy/detail/code/881117/"
+  },
+  {
+    "sector_name": "专用设备",
+    "sector_code": "881118",
+    "detail_url": "http://q.10jqka.com.cn/thshy/detail/code/881118/"
+  },
+  {
+    "sector_name": "半导体",
+    "sector_code": "881121",
+    "detail_url": "http://q.10jqka.com.cn/thshy/detail/code/881121/"
+  },
+  {
+    "sector_name": "光学光电子",
+    "sector_code": "881122",
+    "detail_url": "http://q.10jqka.com.cn/thshy/detail/code/881122/"
+  },
+  {
+    "sector_name": "其他电子",
+    "sector_code": "881123",
+    "detail_url": "http://q.10jqka.com.cn/thshy/detail/code/881123/"
+  },
+  {
+    "sector_name": "消费电子",
+    "sector_code": "881124",
+    "detail_url": "http://q.10jqka.com.cn/thshy/detail/code/881124/"
+  },
+  {
+    "sector_name": "汽车整车",
+    "sector_code": "881125",
+    "detail_url": "http://q.10jqka.com.cn/thshy/detail/code/881125/"
+  },
+  {
+    "sector_name": "汽车零部件",
+    "sector_code": "881126",
+    "detail_url": "http://q.10jqka.com.cn/thshy/detail/code/881126/"
+  },
+  {
+    "sector_name": "汽车服务及其他",
+    "sector_code": "881128",
+    "detail_url": "http://q.10jqka.com.cn/thshy/detail/code/881128/"
+  },
+  {
+    "sector_name": "通信设备",
+    "sector_code": "881129",
+    "detail_url": "http://q.10jqka.com.cn/thshy/detail/code/881129/"
+  },
+  {
+    "sector_name": "计算机设备",
+    "sector_code": "881130",
+    "detail_url": "http://q.10jqka.com.cn/thshy/detail/code/881130/"
+  },
+  {
+    "sector_name": "白色家电",
+    "sector_code": "881131",
+    "detail_url": "http://q.10jqka.com.cn/thshy/detail/code/881131/"
+  },
+  {
+    "sector_name": "黑色家电",
+    "sector_code": "881132",
+    "detail_url": "http://q.10jqka.com.cn/thshy/detail/code/881132/"
+  },
+  {
+    "sector_name": "饮料制造",
+    "sector_code": "881133",
+    "detail_url": "http://q.10jqka.com.cn/thshy/detail/code/881133/"
+  },
+  {
+    "sector_name": "食品加工制造",
+    "sector_code": "881134",
+    "detail_url": "http://q.10jqka.com.cn/thshy/detail/code/881134/"
+  },
+  {
+    "sector_name": "纺织制造",
+    "sector_code": "881135",
+    "detail_url": "http://q.10jqka.com.cn/thshy/detail/code/881135/"
+  },
+  {
+    "sector_name": "服装家纺",
+    "sector_code": "881136",
+    "detail_url": "http://q.10jqka.com.cn/thshy/detail/code/881136/"
+  },
+  {
+    "sector_name": "造纸",
+    "sector_code": "881137",
+    "detail_url": "http://q.10jqka.com.cn/thshy/detail/code/881137/"
+  },
+  {
+    "sector_name": "包装印刷",
+    "sector_code": "881138",
+    "detail_url": "http://q.10jqka.com.cn/thshy/detail/code/881138/"
+  },
+  {
+    "sector_name": "家居用品",
+    "sector_code": "881139",
+    "detail_url": "http://q.10jqka.com.cn/thshy/detail/code/881139/"
+  },
+  {
+    "sector_name": "化学制药",
+    "sector_code": "881140",
+    "detail_url": "http://q.10jqka.com.cn/thshy/detail/code/881140/"
+  },
+  {
+    "sector_name": "中药",
+    "sector_code": "881141",
+    "detail_url": "http://q.10jqka.com.cn/thshy/detail/code/881141/"
+  },
+  {
+    "sector_name": "生物制品",
+    "sector_code": "881142",
+    "detail_url": "http://q.10jqka.com.cn/thshy/detail/code/881142/"
+  },
+  {
+    "sector_name": "医药商业",
+    "sector_code": "881143",
+    "detail_url": "http://q.10jqka.com.cn/thshy/detail/code/881143/"
+  },
+  {
+    "sector_name": "医疗器械",
+    "sector_code": "881144",
+    "detail_url": "http://q.10jqka.com.cn/thshy/detail/code/881144/"
+  },
+  {
+    "sector_name": "电力",
+    "sector_code": "881145",
+    "detail_url": "http://q.10jqka.com.cn/thshy/detail/code/881145/"
+  },
+  {
+    "sector_name": "燃气",
+    "sector_code": "881146",
+    "detail_url": "http://q.10jqka.com.cn/thshy/detail/code/881146/"
+  },
+  {
+    "sector_name": "港口航运",
+    "sector_code": "881148",
+    "detail_url": "http://q.10jqka.com.cn/thshy/detail/code/881148/"
+  },
+  {
+    "sector_name": "公路铁路运输",
+    "sector_code": "881149",
+    "detail_url": "http://q.10jqka.com.cn/thshy/detail/code/881149/"
+  },
+  {
+    "sector_name": "机场航运",
+    "sector_code": "881151",
+    "detail_url": "http://q.10jqka.com.cn/thshy/detail/code/881151/"
+  },
+  {
+    "sector_name": "物流",
+    "sector_code": "881152",
+    "detail_url": "http://q.10jqka.com.cn/thshy/detail/code/881152/"
+  },
+  {
+    "sector_name": "房地产",
+    "sector_code": "881153",
+    "detail_url": "http://q.10jqka.com.cn/thshy/detail/code/881153/"
+  },
+  {
+    "sector_name": "银行",
+    "sector_code": "881155",
+    "detail_url": "http://q.10jqka.com.cn/thshy/detail/code/881155/"
+  },
+  {
+    "sector_name": "保险",
+    "sector_code": "881156",
+    "detail_url": "http://q.10jqka.com.cn/thshy/detail/code/881156/"
+  },
+  {
+    "sector_name": "证券",
+    "sector_code": "881157",
+    "detail_url": "http://q.10jqka.com.cn/thshy/detail/code/881157/"
+  },
+  {
+    "sector_name": "零售",
+    "sector_code": "881158",
+    "detail_url": "http://q.10jqka.com.cn/thshy/detail/code/881158/"
+  },
+  {
+    "sector_name": "贸易",
+    "sector_code": "881159",
+    "detail_url": "http://q.10jqka.com.cn/thshy/detail/code/881159/"
+  },
+  {
+    "sector_name": "旅游及酒店",
+    "sector_code": "881160",
+    "detail_url": "http://q.10jqka.com.cn/thshy/detail/code/881160/"
+  },
+  {
+    "sector_name": "通信服务",
+    "sector_code": "881162",
+    "detail_url": "http://q.10jqka.com.cn/thshy/detail/code/881162/"
+  },
+  {
+    "sector_name": "文化传媒",
+    "sector_code": "881164",
+    "detail_url": "http://q.10jqka.com.cn/thshy/detail/code/881164/"
+  },
+  {
+    "sector_name": "综合",
+    "sector_code": "881165",
+    "detail_url": "http://q.10jqka.com.cn/thshy/detail/code/881165/"
+  },
+  {
+    "sector_name": "军工装备",
+    "sector_code": "881166",
+    "detail_url": "http://q.10jqka.com.cn/thshy/detail/code/881166/"
+  },
+  {
+    "sector_name": "非金属材料",
+    "sector_code": "881167",
+    "detail_url": "http://q.10jqka.com.cn/thshy/detail/code/881167/"
+  },
+  {
+    "sector_name": "工业金属",
+    "sector_code": "881168",
+    "detail_url": "http://q.10jqka.com.cn/thshy/detail/code/881168/"
+  },
+  {
+    "sector_name": "贵金属",
+    "sector_code": "881169",
+    "detail_url": "http://q.10jqka.com.cn/thshy/detail/code/881169/"
+  },
+  {
+    "sector_name": "小金属",
+    "sector_code": "881170",
+    "detail_url": "http://q.10jqka.com.cn/thshy/detail/code/881170/"
+  },
+  {
+    "sector_name": "自动化设备",
+    "sector_code": "881171",
+    "detail_url": "http://q.10jqka.com.cn/thshy/detail/code/881171/"
+  },
+  {
+    "sector_name": "电子化学品",
+    "sector_code": "881172",
+    "detail_url": "http://q.10jqka.com.cn/thshy/detail/code/881172/"
+  },
+  {
+    "sector_name": "小家电",
+    "sector_code": "881173",
+    "detail_url": "http://q.10jqka.com.cn/thshy/detail/code/881173/"
+  },
+  {
+    "sector_name": "厨卫电器",
+    "sector_code": "881174",
+    "detail_url": "http://q.10jqka.com.cn/thshy/detail/code/881174/"
+  },
+  {
+    "sector_name": "医疗服务",
+    "sector_code": "881175",
+    "detail_url": "http://q.10jqka.com.cn/thshy/detail/code/881175/"
+  },
+  {
+    "sector_name": "互联网电商",
+    "sector_code": "881177",
+    "detail_url": "http://q.10jqka.com.cn/thshy/detail/code/881177/"
+  },
+  {
+    "sector_name": "教育",
+    "sector_code": "881178",
+    "detail_url": "http://q.10jqka.com.cn/thshy/detail/code/881178/"
+  },
+  {
+    "sector_name": "其他社会服务",
+    "sector_code": "881179",
+    "detail_url": "http://q.10jqka.com.cn/thshy/detail/code/881179/"
+  },
+  {
+    "sector_name": "石油加工贸易",
+    "sector_code": "881180",
+    "detail_url": "http://q.10jqka.com.cn/thshy/detail/code/881180/"
+  },
+  {
+    "sector_name": "环境治理",
+    "sector_code": "881181",
+    "detail_url": "http://q.10jqka.com.cn/thshy/detail/code/881181/"
+  },
+  {
+    "sector_name": "美容护理",
+    "sector_code": "881182",
+    "detail_url": "http://q.10jqka.com.cn/thshy/detail/code/881182/"
+  },
+  {
+    "sector_name": "农化制品",
+    "sector_code": "881263",
+    "detail_url": "http://q.10jqka.com.cn/thshy/detail/code/881263/"
+  },
+  {
+    "sector_name": "化学纤维",
+    "sector_code": "881264",
+    "detail_url": "http://q.10jqka.com.cn/thshy/detail/code/881264/"
+  },
+  {
+    "sector_name": "塑料制品",
+    "sector_code": "881265",
+    "detail_url": "http://q.10jqka.com.cn/thshy/detail/code/881265/"
+  },
+  {
+    "sector_name": "橡胶制品",
+    "sector_code": "881266",
+    "detail_url": "http://q.10jqka.com.cn/thshy/detail/code/881266/"
+  },
+  {
+    "sector_name": "能源金属",
+    "sector_code": "881267",
+    "detail_url": "http://q.10jqka.com.cn/thshy/detail/code/881267/"
+  },
+  {
+    "sector_name": "工程机械",
+    "sector_code": "881268",
+    "detail_url": "http://q.10jqka.com.cn/thshy/detail/code/881268/"
+  },
+  {
+    "sector_name": "轨交设备",
+    "sector_code": "881269",
+    "detail_url": "http://q.10jqka.com.cn/thshy/detail/code/881269/"
+  },
+  {
+    "sector_name": "元件",
+    "sector_code": "881270",
+    "detail_url": "http://q.10jqka.com.cn/thshy/detail/code/881270/"
+  },
+  {
+    "sector_name": "IT服务",
+    "sector_code": "881271",
+    "detail_url": "http://q.10jqka.com.cn/thshy/detail/code/881271/"
+  },
+  {
+    "sector_name": "软件开发",
+    "sector_code": "881272",
+    "detail_url": "http://q.10jqka.com.cn/thshy/detail/code/881272/"
+  },
+  {
+    "sector_name": "白酒",
+    "sector_code": "881273",
+    "detail_url": "http://q.10jqka.com.cn/thshy/detail/code/881273/"
+  },
+  {
+    "sector_name": "影视院线",
+    "sector_code": "881274",
+    "detail_url": "http://q.10jqka.com.cn/thshy/detail/code/881274/"
+  },
+  {
+    "sector_name": "游戏",
+    "sector_code": "881275",
+    "detail_url": "http://q.10jqka.com.cn/thshy/detail/code/881275/"
+  },
+  {
+    "sector_name": "军工电子",
+    "sector_code": "881276",
+    "detail_url": "http://q.10jqka.com.cn/thshy/detail/code/881276/"
+  },
+  {
+    "sector_name": "电机",
+    "sector_code": "881277",
+    "detail_url": "http://q.10jqka.com.cn/thshy/detail/code/881277/"
+  },
+  {
+    "sector_name": "电网设备",
+    "sector_code": "881278",
+    "detail_url": "http://q.10jqka.com.cn/thshy/detail/code/881278/"
+  },
+  {
+    "sector_name": "光伏设备",
+    "sector_code": "881279",
+    "detail_url": "http://q.10jqka.com.cn/thshy/detail/code/881279/"
+  },
+  {
+    "sector_name": "风电设备",
+    "sector_code": "881280",
+    "detail_url": "http://q.10jqka.com.cn/thshy/detail/code/881280/"
+  },
+  {
+    "sector_name": "电池",
+    "sector_code": "881281",
+    "detail_url": "http://q.10jqka.com.cn/thshy/detail/code/881281/"
+  },
+  {
+    "sector_name": "其他电源设备",
+    "sector_code": "881282",
+    "detail_url": "http://q.10jqka.com.cn/thshy/detail/code/881282/"
+  },
+  {
+    "sector_name": "多元金融",
+    "sector_code": "881283",
+    "detail_url": "http://q.10jqka.com.cn/thshy/detail/code/881283/"
+  },
+  {
+    "sector_name": "环保设备",
+    "sector_code": "881284",
+    "detail_url": "http://q.10jqka.com.cn/thshy/detail/code/881284/"
+  }
+]
